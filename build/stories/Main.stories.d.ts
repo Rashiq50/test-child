85cc5a1f717b8071351da0a3076783987ece7cf9
Rashiq Rahman <rahmanrashiq56@gmail.com>
2023-05-26 23:16:40 +0600

    latest build

--- build/stories/Main.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import App from "../App";
-declare const meta: Meta<typeof App>;
-export default meta;
-type Story = StoryObj<typeof App>;
-export declare const Home: Story;
